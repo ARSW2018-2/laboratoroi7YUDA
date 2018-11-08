ee74f85d2fbc9c148cd2ec518e6a58f29d15e5fb
unknown <2098325@labinfo.is.escuelaing.edu.co>
2018-11-08 18:20:52 -0500

    first commit

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.io.IOException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,12 +10,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
+        private static final String USER_AGENT = "Mozilla/5.0";
+        private static final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=Q1QZFVJQ21K7C6XM";
+
+    
+    
+    
 	@Autowired
 	private CustomerRepository repository;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws IOException {
 		SpringApplication.run(Application.class, args);
+                
 	}
+        
+        
+        public void almacenar(String... args)throws Exception{
+        
+        
+        
+        }
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,6 +39,8 @@ public class Application implements CommandLineRunner {
 		// save a couple of customers
 		repository.save(new Customer("Alice", "Smith"));
 		repository.save(new Customer("Bob", "Smith"));
+		repository.save(new Customer("julia", "lore"));
+                repository.save(new Customer("julia", "Smith"));                   
 
 		// fetch all customers
 		System.out.println("Customers found with findAll():");
@@ -42,6 +60,18 @@ public class Application implements CommandLineRunner {
 		for (Customer customer : repository.findByLastName("Smith")) {
 			System.out.println(customer);
 		}
+                Customer per=repository.findByFirstName("julio");
+                System.out.println("lalalal"+per.toString());
+                
+                List<Customer> er=repository.findByLastName("Smith");
+                //System.out.println("miermos nombe"+repository.findByFirstName("julia"));
+                for (int i=0;i<er.size();i++){
+                    System.out.println("que imprime+"+er.get(i));
+                
+                
+                }
+                
+                
 
 	}
 
